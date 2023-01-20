1b18053f609370c7ec5fbf1a823911278a8b0ee9
Grace Gong <ggong@lcs.on.ca>
2023-01-20 15:20:45 -0500

    Initial Commit

---
+++ ShapeCalculator/ShapeCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapeCalculatorApp.swift
+//  ShapeCalculator
+//
+//  Created by Grace Gong on 2023-01-20.
+//
+
+import SwiftUI
+
+@main
+struct ShapeCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
